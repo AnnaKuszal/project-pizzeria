741bd05e9492991417fedc7651714efea0134c0e
Anna Kuszal <anna.kuszal@o2.pl>
2019-11-10 16:06:35 +0100

    Add fetch() to app.initData

--- src/js/script.js
+++ src/js/script.js
@@ -566,7 +566,7 @@
       const thisApp = this;
 
       for(let productData in thisApp.data.products){
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
@@ -575,13 +575,29 @@
   
       thisApp.data = {};
       const url = settings.db.url + '/' + settings.db.product;
+
+      fetch(url)
+      .then(function(rawResponse){
+        return rawResponse.json();
+      })
+      .then(function(parsedResponse){
+        console.log('parsedResponse', parsedResponse);
+
+        /* save parsedResponse at thisApp.data.products */
+        thisApp.data.products = parsedResponse;
+        
+        /* execute initMenu method */
+        thisApp.initMenu();
+      });
+
+      console.log('thisApp.data', JSON.stringify(thisApp.data));
     },
 
     init: function(){
       const thisApp = this;
 
       thisApp.initData();
-      thisApp.initMenu();
+      
       thisApp.initCart();
 
     },
